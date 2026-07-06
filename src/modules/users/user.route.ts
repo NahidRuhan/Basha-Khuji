9094bcf33d1eb85f78fbf11bbc078cd86f5597a8
NahidRuhan <dilal328@gmail.com>
2026-07-06 16:27:07 +0600

    created auth guard middleware and secured get user endpoint

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { auth } from "../../middleware/auth";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = Router()
 
 router.post("/register",userController.registerUser)
-router.get("/",userController.getAllUser)  //TODO: Auth guard
+router.get("/",auth(UserRole.ADMIN),userController.getAllUser)
 
 export const userRoutes = router;
